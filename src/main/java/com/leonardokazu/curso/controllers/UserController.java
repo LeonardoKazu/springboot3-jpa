d1f20f1b82898c08babb38e288e105f907f23474
Leonardo Kazu <kazuyoshikad@gmail.com>
2024-01-09 10:21:35 -0300

    User entity and resource

---
+++ src/main/java/com/leonardokazu/curso/controllers/UserController.java
@@ -0,0 +1,20 @@
+package com.leonardokazu.curso.controllers;
+
+import com.leonardokazu.curso.entities.User;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User u = new User(1L, "Maria", "maria@gamail.com", "999", "12345");
+        return ResponseEntity.status(HttpStatus.OK).body(u);
+    }
+}
