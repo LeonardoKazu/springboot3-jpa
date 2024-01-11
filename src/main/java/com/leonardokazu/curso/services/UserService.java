a6bd8eb8c4e67b0e70f27125710582ef78ed51e3
Leonardo Kazu <kazuyoshikad@gmail.com>
2024-01-11 07:17:54 -0300

    Exception handling - update

--- src/main/java/com/leonardokazu/curso/services/UserService.java
+++ src/main/java/com/leonardokazu/curso/services/UserService.java
@@ -4,6 +4,7 @@ import com.leonardokazu.curso.entities.User;
 import com.leonardokazu.curso.repositories.UserRepository;
 import com.leonardokazu.curso.services.exceptions.DatabaseException;
 import com.leonardokazu.curso.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -38,9 +39,13 @@ public class UserService {
         }
     }
     public User update(Long id, User obj){
-        User entity = userRepository.getReferenceById(id);
-        updateData(entity, obj);
-        return userRepository.save(entity);
+        try {
+            User entity = userRepository.getReferenceById(id);
+            updateData(entity, obj);
+            return userRepository.save(entity);
+        } catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
